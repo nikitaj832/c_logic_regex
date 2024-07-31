003d28f60d8afc0870fce5705ef6fa4fa48eec57
nikita <nikitajainbhl@fmail.com>
2024-07-31 18:42:19 +0530

    hell0 this is assignment

---
+++ 31july.c
@@ -0,0 +1,62 @@
+// #include<stdio.h>
+// int main()
+// {
+//    printf("hello world");
+//    return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("mahak");
+//     printf("pooja");
+//     printf("maya");
+//     printf("ayushi");
+//     printf("poonam");
+//     return 0;
+// }
+
+//// 2. five country name(no new line):
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("india ");
+//     printf("australia ");
+//     printf("america ");
+//     printf("china ");
+//     printf("nepal");
+//     return 0;
+// }
+
+// 3. five city name(new line):
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("bhilwara\n");
+//     printf("jaipur\n");
+//     printf("chhitor\n");
+//     printf("kota\n");
+//     printf("ajmer");
+//     return 0;
+// }
+
+// //4. five food name(no new line):
+
+#include<stdio.h>
+int main()
+{
+    printf("pizza ");
+    printf("burger ");
+    printf("cheese ");
+    printf("pasta ") ;
+    printf("maggie ");
+    return 0;
+}
+
+
+
+
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("evas eatry\n");
+    printf("mean mug\n");
+    printf("the coffee bar\n");
+    printf("cup of joe\n") ;
+    printf("morning glory cafe");
+    return 0;
+}
