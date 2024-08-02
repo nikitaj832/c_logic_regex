79205bb39877f85b422c5a852c53f4324cac6211
nikita <nikitajainbhl@fmail.com>
2024-08-02 21:34:16 +0530

    c program

--- 2aug.c
+++ 2aug.c
@@ -17,17 +17,47 @@
 
 
 
-#include<stdio.h>
-int main(){
-    int a =100;
-    int b = 300;
-    printf("the value of a is : %d\n",a);
-    printf("the value of b is : %d\n",b);
-    a = a+b;
-    b = a-b;
-    a = a-b;
-    printf("the swap value of a is : %d\n",a);
-    printf("the swap value of b is : %d\n",b);
+// #include<stdio.h>
+// int main(){
+//     int a =100;
+//     int b = 300;
+//     printf("the value of a is : %d\n",a);
+//     printf("the value of b is : %d\n",b);
+//     a = a+b;
+//     b = a-b;
+//     a = a-b;
+//     printf("the swap value of a is : %d\n",a);
+//     printf("the swap value of b is : %d\n",b);
 
- return 0;
-}
+//  return 0;
+// }
+
+//value reassign cncept 
+// #include<stdio.h>
+// int main(){
+//     int a= 12;
+//     a=5;
+//     printf("%d",a);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a= 12;
+//     a=5;
+//     a=8;
+//     a=10;
+//     printf("%d",a);
+//     return 0;
+// }
+
+//copy of value:
+
+// #include<stdio.h>
+// int main(){
+//     int a=4;
+//     int b=a;
+//     printf("%d",a);
+//     printf("%d",b);
+//     return 0;
+// }
