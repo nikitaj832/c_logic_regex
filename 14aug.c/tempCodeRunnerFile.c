ddc7aa57ef9032dbde9e79a2509a88ea3c4f16bd
nikita <nikitajainbhl@fmail.com>
2024-08-19 21:23:13 +0530

    this is code

---
+++ 14aug.c/tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+int main()
+{
+   for(int i=1;i<=4;i++){
+     for(int j=1;j<=5;j++){
+           if (j % 2 == 0){
+                printf("0");}
+            else{
+                printf("1");
+        }}
+       printf("\n");
+   }
+  return 0;
+}
