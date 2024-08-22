8f29844156ea28f16c1441a1b96efe023ee7dfeb
nikita <nikitajainbhl@fmail.com>
2024-08-22 18:45:27 +0530

    this is assignment

---
+++ 21aug.c/pattern.c
@@ -0,0 +1,596 @@
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1 || i==3||j==1 || j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// *****
+// *   *
+// *****
+// *   *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1 || i==3||i==5||j==1 || j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *****
+// *   
+// *   
+// *   
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1 ||i==5||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *****
+//     *
+//     *
+//     *
+// *****
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1 ||i==5||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+// ----------------------------------------------------------------------
+// *   *
+// *   *
+// *   *
+// *   *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==5||j==1||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+// *   *
+// *   *
+// *   *
+// *   *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||j==1||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *****
+// *
+// *****
+// *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||i==3||i==5||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+// *
+// *****
+// *
+// *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||i==3||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *   *
+// *   *
+// *****
+// *   *
+// *   *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==3||j==1||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+//   *
+//   *
+//   *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||i==5||j==3){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *
+// *
+// *
+// *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==5||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+//     *
+//     *
+//     *
+//     *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// *****
+// *
+// *
+// *
+// *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+//     *
+//     *
+//     *
+//     *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==5||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// ******
+//    *
+//   *
+//  *
+// ******
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=5;i++){
+//         for(int j=0;j<=5;j++){
+//             if(i==0||i==4||j== 5-i-1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+//  *
+//   *
+//    *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||i==5 || i==j){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *
+//  *
+//   *
+//    *
+//     *
+// 
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==j){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+//     *
+//    *
+//   *
+//  *
+// *
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=5;i++){
+//         for(int j=0;j<=5;j++){
+//             if(j== 5-i-1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+// **
+// * *
+// *  *
+// *   *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==1||i==j||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+//    **
+//   * *
+//  *  *
+// *   *
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=5;i++){
+//         for(int j=0;j<=5;j++){
+//             if(i==0||j==5-i||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// *   *
+//  *  *
+//   * *
+//    **
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==5||j==i||j==5){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// *   *
+// *  *
+// * *
+// **
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=4;i++){
+//         for(int j=0;j<=4;j++){
+//             if(i==4||j==4-i||j==0){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+// *****
+// **  *
+// * * *
+// *  **
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if(i==5||j==i||j==5||i==1||j==1){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// *****
+// *  **
+// * * *
+// **  *
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=4;i++){
+//         for(int j=0;j<=4;j++){
+//             if(i==4||j==4-i||j==4||i==0||j==0){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *****
+// ** **
+// * * *
+// ** **
+// *****
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=4;i++){
+//         for(int j=0;j<=4;j++){
+//             if(i==4||j==4-i||j==4||i==0||j==0 ||i==j){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *   *
+//  * *
+//   *
+//  * *
+// *   *
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<=4;i++){
+//         for(int j=0;j<=4;j++){
+//             if(j==4-i||i==j){
+//                 printf("*");
+
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
